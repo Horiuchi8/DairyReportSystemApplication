055aff165b7ee2d3e5e5bee11f16940536bf1684
Horiuchi8 <147533337+Horiuchi8@users.noreply.github.com>
2024-04-07 00:14:28 +0900

    Add:add, update, and delete functions

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -1,16 +1,23 @@
 package com.techacademy.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
 import com.techacademy.entity.Employee;
 import com.techacademy.entity.Report;
 import com.techacademy.service.ReportService;
+import com.techacademy.service.UserDetail;
 
 @Controller
 @RequestMapping("reports")
@@ -36,9 +43,22 @@ public class ReportController {
 
     // 日報新規登録画面
     @GetMapping(value = "/add")
-    public String add(@ModelAttribute Report code) {
-
+    public String create(@AuthenticationPrincipal UserDetail userDetail,Report report, Model model) {
+//        System.out.println(userDetails.getEmployee().getName());
+        model.addAttribute("username", userDetail.getEmployee().getName());
+        model.addAttribute("employee", userDetail.getEmployee());
         return "reports/new";
+    }
+
+    //日報新規登録処理
+    @PostMapping(value = "/add")
+    public String add(@AuthenticationPrincipal UserDetail userDetail, Report report, Model model,BindingResult res) {
+        model.addAttribute("employee", userDetail.getEmployee());
+        report.setEmployee(userDetail.getEmployee());
+
+        reportService.saveReport(report);
+
+        return "redirect:/reports";
     }
 
     // 日報詳細画面
@@ -49,14 +69,29 @@ public class ReportController {
         return "reports/detail";
     }
 
-    //従業員更新画面
+    //日報更新画面
     @GetMapping(value = "/{code}/update")
-    public String edit(@PathVariable String code, Model model, Employee employee) {
-
-        if(code != null) {
-        model.addAttribute("employee", reportService.getReport(code));
-        }
+    public String edit(@PathVariable String code, Model model, Report report) {
+        model.addAttribute("report", reportService.getReport(code));
 
         return "reports/edit";
+    }
+
+    //日報更新処理
+    @PostMapping(value = "/{code}/update")
+    public String update(@PathVariable String code, @Validated Report report, BindingResult res, Model model) {
+
+        reportService.updateSave(report, code);
+        //更新ができたら一覧にリダイレクト
+        return "redirect:/reports";
+    }
+
+    //日報削除処理
+    @PostMapping(value = "/{code}/delete")
+    public String delete(@PathVariable String code, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+
+        reportService.delete(code, userDetail);
+
+        return "redirect:/reports";
     }
 }
